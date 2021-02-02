9b471f292dcce81d1d4fd43eb921a9376835afba
Becca Manning <rebm01@hotmail.co.uk>
2021-02-02 23:39:44 +0000

    Add test ui code for full stack app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = 'http://localhost:8080/todos';
+
+const App = () => {
+    const [newTodo, setNewTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    const getTodos = async() => {
+        try {
+            const res = await axios.get(url);
+            if (res.status !== 200) {
+                setTodos(res.data);
+            } else {
+                console.log(`Error code ${res.status}`);
+            }
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
+    const addTodo = async() => {
+        try {
+            const res = await axios.post(url, { todo: newTodo });
+            if (res.status !== 200) {
+                setNewTodo('');
+            } else {
+                console.log(`Error code ${res.status}`);
+            }
+        } catch (err) {
+            console.log(err.message)
+        }
+    };
+
+    const deleteTodo = async(id) => {
+        try {
+            // for delete method, the body has to be like { data: body }
+            const res = await axios.delete(url, { data: { id } });
+            if (res.status !== 200) {
+                getTodos();
+            } else {
+                console.log(`Error code ${res.status}`);
+            }
+        } catch (err) {
+            console.log(err.message)
+        }
+    };
+
+    return (
+        <div>
+            <p>Full stack TODO application to practice coding and deployment.</p>
+
+            <h1>TODOs</h1>
+            <form>
+                <input autoFocus onChange={e => setNewTodo(e.target.value)} value={newTodo} />
+                <button type="submit" onClick={addTodo}>Add TODO</button>
+            </form>
+
+            {todos.map(({ id, todo }, i) => (
+                <div key={i} style={{ display: 'flex' }}>
+                    <button onClick={() => deleteTodo(id)}>x</button>
+                    <p>{todo}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
